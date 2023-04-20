72257dbbd4812169aad536714de1e0369a7bb653
IirisHa <iiris.hare@edu.savonia.fi>
2023-04-20 12:24:02 +0300

    Asiakastaulun labelit

--- R13_MokkiBook/frmAsiakkaat.Designer.cs
+++ R13_MokkiBook/frmAsiakkaat.Designer.cs
@@ -28,25 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.lbAsiakasid = new System.Windows.Forms.Label();
+            this.lbEmail = new System.Windows.Forms.Label();
+            this.lbLahiosoite = new System.Windows.Forms.Label();
+            this.lbSukunimi = new System.Windows.Forms.Label();
+            this.lbEtunimi = new System.Windows.Forms.Label();
+            this.lb = new System.Windows.Forms.Label();
+            this.lbPuhelinnumero = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // label1
+            // lbAsiakasid
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(135, 63);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 16);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
-            
+            this.lbAsiakasid.AutoSize = true;
+            this.lbAsiakasid.Location = new System.Drawing.Point(36, 49);
+            this.lbAsiakasid.Name = "lbAsiakasid";
+            this.lbAsiakasid.Size = new System.Drawing.Size(74, 16);
+            this.lbAsiakasid.TabIndex = 0;
+            this.lbAsiakasid.Text = "Asiakas_id";
+            // 
+            // lbEmail
+            // 
+            this.lbEmail.AutoSize = true;
+            this.lbEmail.Location = new System.Drawing.Point(36, 277);
+            this.lbEmail.Name = "lbEmail";
+            this.lbEmail.Size = new System.Drawing.Size(41, 16);
+            this.lbEmail.TabIndex = 1;
+            this.lbEmail.Text = "Email";
+            // 
+            // lbLahiosoite
+            // 
+            this.lbLahiosoite.AutoSize = true;
+            this.lbLahiosoite.Location = new System.Drawing.Point(36, 227);
+            this.lbLahiosoite.Name = "lbLahiosoite";
+            this.lbLahiosoite.Size = new System.Drawing.Size(69, 16);
+            this.lbLahiosoite.TabIndex = 2;
+            this.lbLahiosoite.Text = "Lähiosoite";
+            // 
+            // lbSukunimi
+            // 
+            this.lbSukunimi.AutoSize = true;
+            this.lbSukunimi.Location = new System.Drawing.Point(36, 183);
+            this.lbSukunimi.Name = "lbSukunimi";
+            this.lbSukunimi.Size = new System.Drawing.Size(61, 16);
+            this.lbSukunimi.TabIndex = 3;
+            this.lbSukunimi.Text = "Sukunimi";
+            // 
+            // lbEtunimi
+            // 
+            this.lbEtunimi.AutoSize = true;
+            this.lbEtunimi.Location = new System.Drawing.Point(36, 139);
+            this.lbEtunimi.Name = "lbEtunimi";
+            this.lbEtunimi.Size = new System.Drawing.Size(50, 16);
+            this.lbEtunimi.TabIndex = 4;
+            this.lbEtunimi.Text = "Etunimi";
+            // 
+            // lb
+            // 
+            this.lb.AutoSize = true;
+            this.lb.Location = new System.Drawing.Point(36, 94);
+            this.lb.Name = "lb";
+            this.lb.Size = new System.Drawing.Size(82, 16);
+            this.lb.TabIndex = 5;
+            this.lb.Text = "Postinumero";
+            // 
+            // lbPuhelinnumero
+            // 
+            this.lbPuhelinnumero.AutoSize = true;
+            this.lbPuhelinnumero.Location = new System.Drawing.Point(36, 322);
+            this.lbPuhelinnumero.Name = "lbPuhelinnumero";
+            this.lbPuhelinnumero.Size = new System.Drawing.Size(96, 16);
+            this.lbPuhelinnumero.TabIndex = 6;
+            this.lbPuhelinnumero.Text = "Puhelinnumero";
             // 
             // frmAsiakkaat
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbPuhelinnumero);
+            this.Controls.Add(this.lb);
+            this.Controls.Add(this.lbEtunimi);
+            this.Controls.Add(this.lbSukunimi);
+            this.Controls.Add(this.lbLahiosoite);
+            this.Controls.Add(this.lbEmail);
+            this.Controls.Add(this.lbAsiakasid);
             this.Name = "frmAsiakkaat";
             this.Text = "frmAsiakkaat";
             this.ResumeLayout(false);
@@ -56,6 +121,12 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbAsiakasid;
+        private System.Windows.Forms.Label lbEmail;
+        private System.Windows.Forms.Label lbLahiosoite;
+        private System.Windows.Forms.Label lbSukunimi;
+        private System.Windows.Forms.Label lbEtunimi;
+        private System.Windows.Forms.Label lb;
+        private System.Windows.Forms.Label lbPuhelinnumero;
     }
 }
