c667ce3ee60d04fa84765a846b20542883a4d45f
Pan T <107498260+PanTheRandomness@users.noreply.github.com>
2023-04-20 11:12:26 +0300

    SQL-pelleilyä

--- R13_MokkiBook/frmAlkunaytto.Designer.cs
+++ R13_MokkiBook/frmAlkunaytto.Designer.cs
@@ -28,57 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.btnVaraukset = new System.Windows.Forms.Button();
+            this.btnMokit = new System.Windows.Forms.Button();
+            this.btnAlueet = new System.Windows.Forms.Button();
+            this.btnAsiakkaat = new System.Windows.Forms.Button();
+            this.btnPalvelut = new System.Windows.Forms.Button();
+            this.btnLaskut = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // btnVaraukset
             // 
-            this.button1.Location = new System.Drawing.Point(68, 42);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(150, 41);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnVaraukset.Location = new System.Drawing.Point(68, 42);
+            this.btnVaraukset.Name = "btnVaraukset";
+            this.btnVaraukset.Size = new System.Drawing.Size(150, 41);
+            this.btnVaraukset.TabIndex = 0;
+            this.btnVaraukset.Text = "Varaukset";
+            this.btnVaraukset.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnMokit
             // 
-            this.button2.Location = new System.Drawing.Point(68, 116);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(150, 41);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnMokit.Location = new System.Drawing.Point(68, 159);
+            this.btnMokit.Name = "btnMokit";
+            this.btnMokit.Size = new System.Drawing.Size(150, 41);
+            this.btnMokit.TabIndex = 1;
+            this.btnMokit.Text = "Mökit";
+            this.btnMokit.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // btnAlueet
             // 
-            this.button3.Location = new System.Drawing.Point(68, 192);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(150, 41);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnAlueet.Location = new System.Drawing.Point(68, 287);
+            this.btnAlueet.Name = "btnAlueet";
+            this.btnAlueet.Size = new System.Drawing.Size(150, 41);
+            this.btnAlueet.TabIndex = 2;
+            this.btnAlueet.Text = "Alueet";
+            this.btnAlueet.UseVisualStyleBackColor = true;
             // 
-            // Form1
+            // btnAsiakkaat
+            // 
+            this.btnAsiakkaat.Location = new System.Drawing.Point(68, 403);
+            this.btnAsiakkaat.Name = "btnAsiakkaat";
+            this.btnAsiakkaat.Size = new System.Drawing.Size(150, 41);
+            this.btnAsiakkaat.TabIndex = 3;
+            this.btnAsiakkaat.Text = "Asiakkaat";
+            this.btnAsiakkaat.UseVisualStyleBackColor = true;
+            // 
+            // btnPalvelut
+            // 
+            this.btnPalvelut.Location = new System.Drawing.Point(436, 42);
+            this.btnPalvelut.Name = "btnPalvelut";
+            this.btnPalvelut.Size = new System.Drawing.Size(150, 41);
+            this.btnPalvelut.TabIndex = 4;
+            this.btnPalvelut.Text = "Palvelut";
+            this.btnPalvelut.UseVisualStyleBackColor = true;
+            // 
+            // btnLaskut
+            // 
+            this.btnLaskut.Location = new System.Drawing.Point(436, 159);
+            this.btnLaskut.Name = "btnLaskut";
+            this.btnLaskut.Size = new System.Drawing.Size(150, 41);
+            this.btnLaskut.TabIndex = 5;
+            this.btnLaskut.Text = "Laskut";
+            this.btnLaskut.UseVisualStyleBackColor = true;
+            // 
+            // frmAlkunaytto
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(926, 505);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.btnLaskut);
+            this.Controls.Add(this.btnPalvelut);
+            this.Controls.Add(this.btnAsiakkaat);
+            this.Controls.Add(this.btnAlueet);
+            this.Controls.Add(this.btnMokit);
+            this.Controls.Add(this.btnVaraukset);
+            this.Name = "frmAlkunaytto";
+            this.Text = "MökkiBook";
+            this.Load += new System.EventHandler(this.frmAlkunaytto_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnVaraukset;
+        private System.Windows.Forms.Button btnMokit;
+        private System.Windows.Forms.Button btnAlueet;
+        private System.Windows.Forms.Button btnAsiakkaat;
+        private System.Windows.Forms.Button btnPalvelut;
+        private System.Windows.Forms.Button btnLaskut;
     }
 }
 
--- R13_MokkiBook/frmAlkunaytto.cs
+++ R13_MokkiBook/frmAlkunaytto.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,18 @@ namespace R13_MokkiBook
         public frmAlkunaytto()
         {
             InitializeComponent();
+        }
+
+        private void frmAlkunaytto_Load(object sender, EventArgs e)
+        {
+            string connetionString;
+            SqlConnection cnn;
+            connetionString = @"Data Source=Village Newbies;uid=root";
+            //connetionString = @"Data Source=WIN-50GP30FGO75(?);Initial Catalog=Village Newbies;User ID=root;Password=Ruutti";
+            cnn = new SqlConnection(connetionString);
+            cnn.Open();
+            MessageBox.Show("Connection Open  !");
+            cnn.Close();
         }
     }
 }
