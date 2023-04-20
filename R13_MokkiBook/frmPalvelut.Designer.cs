d9f46858a515beb26c2f54a3cace3fcc88a08185
Oskari Sand <122367381+Hiekkamyrsky@users.noreply.github.com>
2023-04-20 12:23:33 +0300

    Päivitys

--- R13_MokkiBook/frmPalvelut.Designer.cs
+++ R13_MokkiBook/frmPalvelut.Designer.cs
@@ -33,13 +33,19 @@
             this.checkBox2 = new System.Windows.Forms.CheckBox();
             this.checkBox3 = new System.Windows.Forms.CheckBox();
             this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
             this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.checkBox1.Location = new System.Drawing.Point(245, 52);
+            this.checkBox1.Location = new System.Drawing.Point(104, 68);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(266, 24);
             this.checkBox1.TabIndex = 0;
@@ -62,7 +68,7 @@
             // 
             this.checkBox2.AutoSize = true;
             this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.checkBox2.Location = new System.Drawing.Point(245, 79);
+            this.checkBox2.Location = new System.Drawing.Point(104, 94);
             this.checkBox2.Name = "checkBox2";
             this.checkBox2.Size = new System.Drawing.Size(249, 24);
             this.checkBox2.TabIndex = 2;
@@ -73,7 +79,7 @@
             // 
             this.checkBox3.AutoSize = true;
             this.checkBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.checkBox3.Location = new System.Drawing.Point(245, 136);
+            this.checkBox3.Location = new System.Drawing.Point(104, 147);
             this.checkBox3.Name = "checkBox3";
             this.checkBox3.Size = new System.Drawing.Size(249, 24);
             this.checkBox3.TabIndex = 4;
@@ -84,7 +90,7 @@
             // 
             this.checkBox4.AutoSize = true;
             this.checkBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.checkBox4.Location = new System.Drawing.Point(245, 109);
+            this.checkBox4.Location = new System.Drawing.Point(104, 120);
             this.checkBox4.Name = "checkBox4";
             this.checkBox4.Size = new System.Drawing.Size(266, 24);
             this.checkBox4.TabIndex = 3;
@@ -92,11 +98,80 @@
             this.checkBox4.UseVisualStyleBackColor = true;
             this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(679, 406);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 32);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Varaa";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 406);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 32);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Takaisin";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.checkBox5.Location = new System.Drawing.Point(381, 147);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(249, 24);
+            this.checkBox5.TabIndex = 10;
+            this.checkBox5.Text = "Lemmikkimaksu 35 €/viipymä";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.checkBox6.Location = new System.Drawing.Point(381, 120);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(266, 24);
+            this.checkBox6.TabIndex = 9;
+            this.checkBox6.Text = "Polkupyörän varaus - 12€/pyörä";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.checkBox7.Location = new System.Drawing.Point(381, 94);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(249, 24);
+            this.checkBox7.TabIndex = 8;
+            this.checkBox7.Text = "Lemmikkimaksu 35 €/viipymä";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.checkBox8.Location = new System.Drawing.Point(381, 68);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(266, 24);
+            this.checkBox8.TabIndex = 7;
+            this.checkBox8.Text = "Polkupyörän varaus - 12€/pyörä";
+            this.checkBox8.UseVisualStyleBackColor = true;
+            // 
             // frmPalvelut
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.checkBox7);
+            this.Controls.Add(this.checkBox8);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.checkBox3);
             this.Controls.Add(this.checkBox4);
             this.Controls.Add(this.checkBox2);
@@ -116,5 +191,11 @@
         private System.Windows.Forms.CheckBox checkBox2;
         private System.Windows.Forms.CheckBox checkBox3;
         private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox checkBox8;
     }
 }
