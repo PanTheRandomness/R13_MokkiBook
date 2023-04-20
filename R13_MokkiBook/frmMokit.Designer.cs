5b5830677d7ab4c439d1b8963cbd9e45b71dc053
hhannaee <hannakarkkainen@hotmail.com>
2023-04-20 11:47:46 +0300

    testi2

--- R13_MokkiBook/frmMokit.Designer.cs
+++ R13_MokkiBook/frmMokit.Designer.cs
@@ -29,31 +29,42 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(65, 94);
+            this.button1.Location = new System.Drawing.Point(48, 371);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(133, 47);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(48, 92);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(144, 22);
+            this.textBox1.TabIndex = 1;
+            // 
             // frmMokit
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "frmMokit";
             this.Text = "Mokit";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
