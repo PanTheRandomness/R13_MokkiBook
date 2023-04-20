47c5c0e7d573d59ee81bd33b798c69654a3ecafe
Pan T <107498260+PanTheRandomness@users.noreply.github.com>
2023-04-20 10:56:14 +0300

    fromien luonti

--- R13_MokkiBook/frmAlkunaytto.Designer.cs
+++ R13_MokkiBook/frmAlkunaytto.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace R13_MokkiBook
 {
-    partial class Form1
+    partial class frmAlkunaytto
     {
         /// <summary>
         /// Required designer variable.
@@ -30,6 +30,7 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -50,11 +51,21 @@
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(68, 192);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 41);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(926, 505);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -67,6 +78,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- R13_MokkiBook/frmAlueet.cs
+++ R13_MokkiBook/frmAlueet.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace R13_MokkiBook
 {
-    public partial class Form1 : Form
+    public partial class frmAlueet : Form
     {
-        public Form1()
+        public frmAlueet()
         {
             InitializeComponent();
         }
