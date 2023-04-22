9ffeab59d5acbe70891c48a911110f3d9954d711
Pan T <107498260+PanTheRandomness@users.noreply.github.com>
2023-04-22 16:36:51 +0300

    Revert "Querytutkintaa"
    This reverts commit 972441a2db269ab63dbbb3c001255b4b67d13de6.

--- R13_MokkiBook/frmVaraukset.cs
+++ R13_MokkiBook/frmVaraukset.cs
@@ -25,8 +25,6 @@ namespace R13_MokkiBook
 
         private void frmVaraukset_Load(object sender, EventArgs e)
         {
-            // TODO: This line of code loads data into the 'varauksetData.Mokin_Alue' table. You can move, or remove it, as needed.
-            this.mokin_AlueTableAdapter.Fill(this.varauksetData.Mokin_Alue);
             // TODO: This line of code loads data into the 'varauksetData.varauksen_palvelut' table. You can move, or remove it, as needed.
             this.taVaraus.Fill(this.varauksetData.varaus);
         }
@@ -60,7 +58,7 @@ namespace R13_MokkiBook
 
         private void cbAlue_SelectedIndexChanged(object sender, EventArgs e)
         {
-            //TÄMÄ PITÄÄ SELVITTÄÄ==>> QUERY?? MUTTA EI PRIORITEETTI
+            //EI ALUETTA KOKO DATASETISSÄ??
         }
 
         private void dtpLoppu_ValueChanged(object sender, EventArgs e)
