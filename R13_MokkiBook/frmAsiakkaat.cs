2816df9506dbb35445d83b46fff0bb39be52092f
IirisHa <iiris.hare@edu.savonia.fi>
2023-05-02 14:18:14 +0300

    Lisää-toiminnon muokkausta, asiakkaat

--- R13_MokkiBook/frmAsiakkaat.cs
+++ R13_MokkiBook/frmAsiakkaat.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Odbc;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,39 +13,64 @@ namespace R13_MokkiBook
 {
     public partial class frmAsiakkaat : Form
     {
-        public List<Asiakas> asiakkaat;
-
         public frmAsiakkaat()
         {
             InitializeComponent();
-            asiakkaat = new List<Asiakas>();
+           
         }
-
         private void frmAsiakkaat_Load(object sender, EventArgs e)
         {
             // TODO: This line of code loads data into the 'dataSet1.asiakas' table. You can move, or remove it, as needed.
             this.asiakasTableAdapter.Fill(this.dataSet1.asiakas);
 
         }
-       
+      
+        public List<Asiakas> asiakkaat = new List<Asiakas>();
         public void LisaaAsiakas()
         {
-            Asiakas a = new Asiakas();
-            a.asiakas_id = int.Parse(tbAsiakasid.Text);
-            a.postinro = tbPostiNro.Text;
-            a.etunimi = tbEtunimi.Text;
-            a.sukunimi = tbSukunimi.Text;
-            a.lahiosoite = tbLahiosoite.Text;
-            a.email = tbEmail.Text;
-            a.puhelinnro = tbPuhelinnro.Text;
-            asiakkaat.Add(a);
+            string connectionString = "Dsn=Village Newbies;uid=root";
+            string query = "SELECT * FROM asiakkaat";
+
+            try
+            {
+
+                using (OdbcConnection connection = new OdbcConnection(connectionString))
+                {
+                    connection.Open();
+                    using (OdbcCommand command = new OdbcCommand(query, connection))
+                    {
+                        using (OdbcDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Asiakas a = new Asiakas();
+                                a.asiakas_id = reader.GetInt32(0);
+                                a.postinro = reader.GetString(1);
+                                a.etunimi = reader.GetString(1);
+                                a.sukunimi = reader.GetString(1);
+                                a.lahiosoite = reader.GetString(1);
+                                a.email = reader.GetString(1);
+                                a.puhelinnro = reader.GetString(1);
+
+                                asiakkaat.Add(a);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred. Please try again later.");
+            }
         }
 
-        //Asiakaslista datagridview:hyn
-
-        private void btnLisaa_Click(object sender, EventArgs e)
+       private void btnPoista_Click(object sender, EventArgs e)
         {
-            LisaaAsiakas();
+
+            int rowindex = dgvAsiakkaat.CurrentRow.Index;
+
+            asiakkaat.RemoveAt(rowindex);
+
             dgvAsiakkaat.DataSource = null;
             dgvAsiakkaat.DataSource = asiakkaat;
 
